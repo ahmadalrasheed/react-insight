c3def5a2c23ea924896331f325ae3e1b88f8574b
Ahmad Al Rasheed <ahmadalrasheed@Ahmads-MacBook-Pro-2.local>
2025-04-16 17:28:24 +0300

    Updates

---
+++ precache-manifest.bce7c8666b99332f2e09b41d711f0dd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a521eb624187351a70c95182701e81c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36ccb0387930faf225fb",
+    "url": "./static/css/main.2a6b0fef.chunk.css"
+  },
+  {
+    "revision": "6a7b730cf93855bb45ef",
+    "url": "./static/js/2.be641efd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.be641efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ccb0387930faf225fb",
+    "url": "./static/js/main.75ac6652.chunk.js"
+  },
+  {
+    "revision": "60f63c91ae3a496fccd9",
+    "url": "./static/js/runtime-main.73f74167.js"
+  }
+]);
